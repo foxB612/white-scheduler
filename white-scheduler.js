3b82aa50e49b25cbed75bb3666ebfe7d0969cf4d
foxb612 <cmb612@outlook.com>
2020-09-08 00:26:35 +0800

    Implement event listeners for clicking courses

--- white-scheduler.js
+++ white-scheduler.js
@@ -10,13 +10,13 @@
 
 (function() {
     'use strict';
-    var table = document.getElementById('ACE_DERIVED_CLASS_S_GROUPBOX1');
+    let table = document.getElementById('ACE_DERIVED_CLASS_S_GROUPBOX1');
     if (table) {
-        var newTd = document.createElement('td');
+        let newTd = document.createElement('td');
         newTd.setAttribute('rowspan', '3');
         newTd.setAttribute('valign', 'top');
         newTd.align = 'left';
-        var div = document.createElement('div');
+        let div = document.createElement('div');
         div.style = `
             margin: 0;
             font-size: 9pt;
@@ -24,20 +24,20 @@
         `;
         div.classList.add('PSPUSHBUTTON', 'Left');
         div.onmouseover = function(e) {
-            var div = e.currentTarget;
+            let div = e.currentTarget;
             div.style.background = '#fad9a5';
         };
         div.onmouseout = function(e) {
-            var div = e.currentTarget;
+            let div = e.currentTarget;
             div.style.background = null;
         };
         div.onclick = main;
-        var img = document.createElement('img');
+        let img = document.createElement('img');
         img.src = 'https://image.flaticon.com/icons/svg/185/185675.svg';
         img.height = 20;
         img.style = 'vertical-align: middle';
         div.appendChild(img);
-        var span = document.createElement('span');
+        let span = document.createElement('span');
         span.innerHTML = '点我';
         span.style.backgroundColor = 'Transparent';
         div.appendChild(span);
@@ -47,5 +47,41 @@
 })();
 
 function main(e) {
-    alert(e.currentTarget);
+    let table = document.getElementById('WEEKLY_SCHED_HTMLAREA');
+    table = table.children[2];
+    let row = 0;
+    for (let tr of table.children) {
+        if (row % 2 == 0) {
+            // odd row (head not count)
+            // skip first row (table head)
+            if (row != 0) {
+                for (let td of tr.children) {
+                    // skip the first column (time)
+                    if (td.children.length > 0) {
+                        td.style.cursor = 'pointer';
+                        td.onclick = addLink;
+                    }
+                }
+            }
+        }
+        else {
+            // even row
+            let column = 0;
+            for (let td of tr.children) {
+                // skip the first column (time)
+                if (column != 0 && td.children.length > 0) {
+                    td.style.cursor = 'pointer';
+                    td.onclick = addLink;
+                }
+                column++;
+            }
+        }
+        row++;
+    }
+    alert('Now click on courses to add links.');
+}
+
+function addLink(e) {
+    var info = e.currentTarget.firstChild.innerHTML;
+    alert("You clicked " + info.substr(0, info.indexOf('<br>')));
 }
